5686de9088171b2562f68ba48c6759feb1608478
mk <kanzakimiki.12@gmail.com>
2025-01-10 12:23:44 +0900

    mv image画像追加

---
+++ lp/src/app/components/Mv.tsx
@@ -0,0 +1,13 @@
+import styles from "./Mv.module.scss";
+// import Link from "next/link";
+// import Image from "next/image";
+
+import "../styles/colors.scss";
+
+export default function Mv() {
+  return (
+    <>
+      <div className={styles.mvContainer}></div>
+    </>
+  );
+}
--- lp/src/app/page.tsx
+++ lp/src/app/page.tsx
@@ -2,11 +2,14 @@ import styles from "./page.module.scss";
 import "./styles/colors.scss";
 
 import Header from "./components/Header";
+import Mv from "./components/Mv";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <Mv />
+      <main className={styles.mainContainer}></main>
     </>
   );
 }
